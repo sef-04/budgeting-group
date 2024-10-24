9fdda08b478f512d5765f74ebebd84903ffa8105
Nyyline <nicolettecborre@gmail.com>
2024-10-25 01:50:19 +0800

    DASH MAAYOS SIGN OUT

--- budget/src/component/pages/Dashboard.js
+++ budget/src/component/pages/Dashboard.js
@@ -1,42 +1,37 @@
-import { Link, useMatch, useResolvedPath, useNavigate  } from "react-router-dom";
+import { Link, useMatch, useResolvedPath, useNavigate } from "react-router-dom";
 import '../css/Dashboard.css';
-import React from 'react'
-
+import React from 'react';
 
 export default function Dashboard() {
     const navigate = useNavigate();
 
-    const handleSignOut = () => { 
-        navigate("/login"); 
+    const handleSignOut = () => {
+        navigate("/login");
     };
-
 
     return (
         <div id="navBar-dash">
-            <img src='./elements/2.png' id='logo' alt='Logo' />
-            <nav className="nav">
-                <ul className="menu">
-                    <CustomLink to="/dashboard">Dashboard</CustomLink>
-                    <CustomLink to="/budget">Budget</CustomLink>
-                    <CustomLink to="/expense">Expense</CustomLink>
-                </ul>
+            <header>
+                <img src='./elements/2.png' id='logo' alt='Logo' />
+                <nav className="nav">
+                    <ul>
+                        <CustomLink to="/dashboard">Dashboard</CustomLink>
+                        <CustomLink to="/budget">Budget</CustomLink>
+                        <CustomLink to="/expense">Expense</CustomLink>
+                    </ul>
+                </nav>
                 <button id="sign" onClick={handleSignOut}>Signout</button>
-            </nav>
-            
-            
+            </header>
             <div>
-            <h1>Dashboard</h1>
+                <h1>Dashboard</h1>
             </div>
         </div>
     );
 }
 
-
-// CustomLink function to handle active state styling
 function CustomLink({ to, children, ...props }) {
     const resolvedPath = useResolvedPath(to);
     const isActive = useMatch({ path: resolvedPath.pathname, end: true });
-
 
     return (
         <li className={isActive ? "active" : ""}>
