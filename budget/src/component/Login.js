e350c33a3c1446b4680d85fd088ea596ff3eb8f2
Neil Simon A. Panganiban <neilorion3@gmail.com>
2024-10-26 00:27:56 +0800

    Login Username to Dashboard
    Nilagyan ko lang Comment - Neil

--- budget/src/component/Login.js
+++ budget/src/component/Login.js
@@ -30,7 +30,7 @@ const Login = () => {
           toast.error("The password is incorrect.");
         } else if (response.data === "Login Successful") {
           toast.success("Login Successful!")
-          localStorage.setItem('username', username);
+          localStorage.setItem('username', username); //storage of Username for Dashboard Usage
           setTimeout(() => {
             navigate("/dashboard");
           }, 1000);
